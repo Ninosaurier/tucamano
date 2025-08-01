0756d38daf84e7ada0cff7468f0452b0bbcdb092
Ninosaurier <provenzano.a@gmx.net>
2025-08-01 16:40:06 +0200

    Added the splash screen and added a new home page and moved it in pages

---
+++ src/app/pages/splash/splash.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCol, IonRow, IonGrid } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { ROUTE_PATHS } from 'src/app/route-paths';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCol, IonRow, IonGrid]
+})
+export class SplashPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.router.navigateByUrl(ROUTE_PATHS.HOME, { replaceUrl: true });
+    }, 3000); // 3000 ms = 3 Sekunden
+  }
+
+}
---
+++ src/app/route-paths.ts
@@ -0,0 +1,4 @@
+export const ROUTE_PATHS = {
+  HOME: '/home',
+  SPLASH: '/splash',
+};
